3524147e22af675f63e74dcb92df7692b6325641
Linguinii <46493825+Linguinii@users.noreply.github.com>
2022-10-05 14:36:59 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -5,13 +5,17 @@ var isDark = true;
 
 function setCSS(cssLinkIndex) {
     if (document.cookie.length != 0) {
+        console.log(document.cookie.length + " saved cookie(s)");
         var array = document.cookie.split("=");
         for (let index = 0; index < array.length; index+=2) {
+            console.log(array[index]);
             if(array[index] == "dark") {
                 isDark = array[index + 1]
                 break;
             }
         }
+    } else {
+        console.log("no saved cookies");
     }
     
     if (isDark) {
@@ -28,8 +32,6 @@ function setCSS(cssLinkIndex) {
     newlink.setAttribute("href", current_css);
 
     document.getElementsByTagName("head").item(cssLinkIndex).replaceChild(newlink, oldlink);
-
-    console.log("old "+ oldlink + " new " + newlink);
 }
 
 function changeCSS(cssLinkIndex) {
@@ -52,6 +54,50 @@ function changeCSS(cssLinkIndex) {
     newlink.setAttribute("href", current_css);
 
     document.getElementsByTagName("head").item(cssLinkIndex).replaceChild(newlink, oldlink);
-
-    console.log("old "+ oldlink + " new " + newlink);
 }
+
+let slideIndex_0 = 1;
+let slideIndex_1 = 1;
+let slideIndex_2 = 1;
+showSlides_0(slideIndex_0);
+showSlides_1(slideIndex_1);
+showSlides_2(slideIndex_2);
+
+function nextSlide() {
+  showSlides_0(slideIndex_0 += 1);
+  showSlides_1(slideIndex_1 += 1);
+  showSlides_2(slideIndex_2 += 1);
+}
+
+function showSlides_0(n) {
+  let slides = document.getElementsByClassName("slide_0");
+  if (n > slides.length) {slideIndex_0 = 1}
+  if (n < 1) {slideIndex_0 = slides.length}
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  console.log(slides.length);
+  slides[slideIndex_0-1].style.display = "block";
+}
+
+function showSlides_1(n) {
+    let slides = document.getElementsByClassName("slide_1");
+    if (n > slides.length) {slideIndex_1 = 1}
+    if (n < 1) {slideIndex_1 = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex_1-1].style.display = "block";
+}
+
+function showSlides_2(n) {
+    let slides = document.getElementsByClassName("slide_2");
+    if (n > slides.length) {slideIndex_2 = 1}
+    if (n < 1) {slideIndex_2 = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex_2-1].style.display = "block";
+}
+
+setInterval(function(){ nextSlide(); }, 5000);
